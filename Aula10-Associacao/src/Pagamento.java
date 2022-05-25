545341d94de9017d3713fdc791fcc6fc543ac927
WILLDEGLAN GOMES DA SILVA <00257309322@ifto.local>
2022-05-25 20:32:47 -0300

    terceiro commit

--- Aula10-Associacao/src/Pagamento.java
+++ Aula10-Associacao/src/Pagamento.java
@@ -1,3 +1,17 @@
 public class Pagamento {
     
+    private TipoPagamento tipoPagamento;
+    public TipoPagamento getTipoPagamento() {
+        return this.tipoPagamento;
+    }
+    public void setTipoPagamento(TipoPagamento tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " tipoPagamento='" + getTipoPagamento() + "'" +
+            "}";
+    }
 }
